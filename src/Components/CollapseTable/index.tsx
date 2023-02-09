343672f67e64258109560980fb0a35076095717a
gaabscps <gaabscps@gmail.com>
2023-02-09 09:58:10 -0300

    🚧 CollapseTable testing putting table inside both collapse and title

--- src/Components/CollapseTable/index.tsx
+++ src/Components/CollapseTable/index.tsx
@@ -2,9 +2,15 @@ import React, { useState } from "react";
 
 export interface CollapseTableProps {
   containerTitle: any;
+  containerData: any;
+  containerData2?: any;
 }
 
-export const CollapseTable: React.FC = ({ containerData }: any) => {
+export const CollapseTable: React.FC = ({
+  containerData,
+  containerData2,
+  contentData,
+}: any) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const containerDatas = [
@@ -29,24 +35,75 @@ export const CollapseTable: React.FC = ({ containerData }: any) => {
       data: "10/12/2022",
     },
   ];
+  const containerDatas2 =
+    [
+      // {
+      //   title: "Data",
+      //   data: "10/12/2022",
+      // },
+      // {
+      //   title: "Data",
+      //   data: "10/12/2022",
+      // },
+      // {
+      //   title: "Data",
+      //   data: "10/12/2022",
+      // },
+      // {
+      //   title: "Data",
+      //   data: "10/12/2022",
+      // },
+      // {
+      //   title: "Data",
+      //   data: "10/12/2022",
+      // },
+    ] || [];
+
+  const contentTitle =
+    [
+      {
+        title: "Data",
+        data: "10/12/2022",
+      },
+      {
+        title: "Data",
+        data: "10/12/2022",
+      },
+      {
+        title: "Data",
+        data: "10/12/2022",
+      },
+      {
+        title: "Data",
+        data: "10/12/2022",
+      },
+      {
+        title: "Data",
+        data: "10/12/2022",
+      },
+    ] || [];
 
   return (
     <>
       <div>
         <div>
-          <div
-            style={{
-              gridTemplateColumns:
-                "repeat(" + (containerDatas.length + 1) + ", 1fr)",
-            }}
-            className="containerTitle"
-          >
-            {containerDatas.map((container: any, index: any) => (
-              <div>{container.title}</div>
-            ))}
+          {containerDatas2 && containerDatas2.length > 0 ? (
+            <div className="collapseTable-title">Titulo</div>
+          ) : (
+            <div
+              style={{
+                gridTemplateColumns:
+                  "repeat(" + (containerDatas.length + 1) + ", 1fr)",
+              }}
+              className="containerTitle"
+            >
+              {containerDatas.map((container: any, index: any) => (
+                <div>{container.title}</div>
+              ))}
 
-            <div className="containerArrow"></div>
-          </div>
+              <div className="containerArrow"></div>
+            </div>
+          )}
 
           <div
             onClick={() => {
@@ -57,13 +114,30 @@ export const CollapseTable: React.FC = ({ containerData }: any) => {
               gridTemplateColumns:
                 "repeat(" + (containerDatas.length + 1) + ", 1fr)",
             }}
-            className="collapseTable-container containerTitle"
+            className="collapseTable-container action-icon containerTitle"
           >
             {containerDatas.map((container: any, index: any) => (
               <div>{container.data}</div>
             ))}
+            <div
+              style={
+                containerDatas2 && containerDatas2.length > 0
+                  ? {
+                      position: "relative",
+                      right: "0px",
+                      top: "25px",
+                    }
+                  : {}
+              }
+              className="containerArrow"
+            >
+              X
+            </div>
+            {containerDatas2.map((container: any, index: any) => (
+              <div>{container.data}</div>
+            ))}
 
-            <div className="containerArrow">X</div>
+            {/* <div className="containerArrow">X</div> */}
           </div>
 
           <div
@@ -72,13 +146,15 @@ export const CollapseTable: React.FC = ({ containerData }: any) => {
                 "repeat(" + (containerDatas.length + 1) + ", 1fr)",
             }}
             className={`
-            ${!isOpen ? "collapseTable--open" : "collapseTable--hidden"}
+            ${isOpen ? "collapseTable--open" : "collapseTable--hidden"}
             collapseTable-container containerTitle
             `}
           >
-            {containerDatas.map((container: any, index: any) => (
+            {contentTitle.map((content: any, index: any) => (
               <div className="">
-                <div>{container.data}</div>
+                <div className="contentTitle">{content.title}</div>
+                <div></div>
+                <div>{content.data}</div>
                 <div></div>
               </div>
             ))}
